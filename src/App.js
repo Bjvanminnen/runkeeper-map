a8b516445828e9b576528006d9f55974080a4357
Brent Van Minnen <bjvanminnen@gmail.com>
2017-09-04 13:25:38 -0700

    draw route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 /* global google*/
 import React, { Component } from 'react';
+import pointData from './pointData';
 
 const styles = {
   map: {
@@ -10,15 +11,26 @@ const styles = {
 
 class App extends Component {
   componentDidMount() {
-    var uluru = {lat: -25.363, lng: 131.044};
+    var flightPlanCoordinates = pointData.points.map(p => ({
+      lat: p.latitude,
+      lng: p.longitude,
+    }));
+
     var map = new google.maps.Map(this.mapNode, {
-      zoom: 4,
-      center: uluru
+      zoom: 14,
+      center: flightPlanCoordinates[0]
     });
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
+
+
+    var flightPath = new google.maps.Polyline({
+      path: flightPlanCoordinates,
+      geodesic: true,
+      strokeColor: '#FF0000',
+      strokeOpacity: 1.0,
+      strokeWeight: 2
     });
+
+    flightPath.setMap(map);
   }
 
   render() {
