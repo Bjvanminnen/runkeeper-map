19db8c8b904b70b54ed0695f8ee9afb97968722f
Brent Van Minnen <bjvanminnen@gmail.com>
2017-09-04 16:21:34 -0700

    MapPath component

--- src/App.js
+++ src/App.js
@@ -1,74 +1,49 @@
-/* global google*/
 import React, { Component } from 'react';
 import pointData from './pointData';
+import $ from 'jquery';
+import MapPath from './MapPath';
 
-const styles = {
-  map: {
-    height: 400,
-    width: 400
-  }
-};
+/**
+ * Parses HTML given to us by hitting the activity page to extract the tripUuid
+ */
+function extractTripUuid(html) {
+  const re = /runkeeper:\/\/\?view=activity&tripuuid=(.*)&deepLinkSource=twitterCard/;
+  const content = $(html).filter("meta[name='twitter:app:url:iphone']").attr('content');
+  return re.exec(content)[1];
+}
 
 class App extends Component {
-  componentDidMount() {
-    fetch('http://localhost:8080/activitiesByDateRange?userName=2104721070&startDate=Aug-01-2017')
-      .then(response => response.json())
-      .then(json => console.log(json));
+  // componentDidMount() {
+  //   fetch('http://localhost:8080/activitiesByDateRange?userName=2104721070&startDate=Aug-01-2017')
+  //     .then(response => response.json())
+  //     .then(json => console.log(json));
 
-    fetch('http://localhost:8080/user/2104721070/activity/1048000416')
-      .then(response => response.text())
-      .then(text => console.log(text));
+  //   fetch('http://localhost:8080/user/2104721070/activity/1048000416')
+  //     .then(response => response.text())
+  //     .then(text => {
+  //       extractTripUuid(text);
+  //     });
 
-    fetch('http://localhost:8080/ajax/pointData?tripUuid=ef5af214-f7f2-468c-80cb-9b09ef406029')
-      .then(response => response.json())
-      .then(json => console.log(json));
-
-
-    // $.ajax({
-    //   method: 'GET',
-    //   url: 'https://runkeeper.com/ajax/pointData',
-    //   // jsonp: 'callback',
-    //   // dataType: 'jsonp', //change the datatype to 'jsonp' works in most cases
-    //   // contentType: 'application/json',
-    //   data: {
-    //     tripUuid: 'ef5af214-f7f2-468c-80cb-9b09ef406029'
-    //   }
-    // }).done(data => {
-    //   console.log('succes', data);
-    // }).fail(jqXHR => {
-    //   console.log('error');
-    //   console.log(jqXHR.responseText);
-    // });
-
-    // var flightPlanCoordinates = pointData.points.map(p => ({
-    //   lat: p.latitude,
-    //   lng: p.longitude,
-    // }));
-
-    // var map = new google.maps.Map(this.mapNode, {
-    //   zoom: 14,
-    //   center: flightPlanCoordinates[0]
-    // });
-
-
-    // var flightPath = new google.maps.Polyline({
-    //   path: flightPlanCoordinates,
-    //   geodesic: true,
-    //   strokeColor: '#FF0000',
-    //   strokeOpacity: 1.0,
-    //   strokeWeight: 2
-    // });
-
-    // flightPath.setMap(map);
-  }
+  //   fetch('http://localhost:8080/ajax/pointData?tripUuid=ef5af214-f7f2-468c-80cb-9b09ef406029')
+  //     .then(response => response.json())
+  //     .then(json => console.log(json));
+  // }
 
   render() {
+    const path1 = pointData.paths[0].points.map(p => ({
+      lat: p.latitude,
+      lng: p.longitude,
+    }));
+
+    const path2 = pointData.paths[1].points.map(p => ({
+      lat: p.latitude,
+      lng: p.longitude,
+    }));
+
+
     return (
       <div>
-        <div
-          ref={mapNode => this.mapNode = mapNode}
-          style={styles.map}
-        />
+        <MapPath paths={[path1, path2]}/>
       </div>
     );
   }
---
+++ src/MapPath.js
@@ -0,0 +1,51 @@
+/* global google*/
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const styles = {
+  map: {
+    height: 600,
+    width: 600
+  }
+};
+
+export default class MapPath extends Component {
+  static propTypes = {
+    paths: PropTypes.arrayOf(
+      PropTypes.arrayOf(PropTypes.shape({
+        lat: PropTypes.number.isRequired,
+        lng: PropTypes.number.isRequired,
+      }))
+    )
+  };
+
+  componentDidMount() {
+    const { paths } = this.props;
+
+    const map = new google.maps.Map(this.mapNode, {
+      zoom: 14,
+      center: paths[0][0]
+    });
+
+    paths.forEach(path => {
+      const flightPath = new google.maps.Polyline({
+        path,
+        geodesic: true,
+        strokeColor: '#FF0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+      });
+
+      flightPath.setMap(map);
+    });
+  }
+
+  render() {
+    return (
+      <div
+        ref={mapNode => this.mapNode = mapNode}
+        style={styles.map}
+      />
+    );
+  }
+}
